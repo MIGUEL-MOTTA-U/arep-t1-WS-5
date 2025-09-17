341f6766bcf2415e64e611e07f4a3a3b2abb6fde
unknown <miguel.motta-u@labinfo.is.escuelaing.edu.co>
2025-09-17 11:15:36 -0500

    first commit

---
+++ src/main/java/eci/edu/co/jpa_ws_arep/JpaWsArepApplication.java
@@ -0,0 +1,13 @@
+package eci.edu.co.jpa_ws_arep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaWsArepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaWsArepApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eci/edu/co/jpa_ws_arep/JpaWsArepApplicationTests.java
@@ -0,0 +1,13 @@
+package eci.edu.co.jpa_ws_arep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaWsArepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
